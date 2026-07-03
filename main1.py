ddd769a363ce94d2cc2008114f89eb11811083b0
supercodertech355 <yajat21056@gmail.com>
2026-07-03 20:00:48 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,7 @@
+string = input("please enter your own string :")
+
+string2 = ("")
+for i in string:
+    string2 = i + string2
+print("/nthe orginal string =",string2)
+print("the reversed string",string2)
