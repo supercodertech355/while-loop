08335082b5f2b39ee46a7dc257ee6d30de7d6d57
supercodertech355 <yajat21056@gmail.com>
2026-07-05 11:04:28 +0530

    Add files via upload

---
+++ project diffactivitynestedcondintionas.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if 10 <= age <= 20:
+    print("Welcome! You are allowed inside.")
+else:
+    print("Sorry, you are not allowed inside. Age must be between 10 and 20.")
